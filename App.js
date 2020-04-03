f9e9fa52e4cbad1a0bf6580478c1789f0a5f43c8
Nekika <hugo.jahnke@gmail.com>
2020-04-04 01:21:34 +0200

    Changing files name

---
+++ App.js
@@ -0,0 +1,15 @@
+const API_CONF = require('./config/API')
+const App = require('./src/Weatherbot');
+const bot = new App();
+
+bot.on('ready', async () => {
+    await bot.init();
+})
+
+bot.on('message', message => {
+    bot.treat(message)
+})
+
+bot.login(API_CONF.client.token)
+    .then(() => console.log(`Logged in as ${bot.user.tag}!`))
+    .catch(err => { throw err })
