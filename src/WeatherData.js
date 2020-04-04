37691e480426b5d47338e08a198bca113049d47f
Nekika <hugo.jahnke@gmail.com>
2020-04-04 02:26:04 +0200

    Changing name & Refacto & Adding country feature
    - Refacto It was stupid to divide Zipcode and Cityname because it's used
    the same way in the url. Now we have an unique method to get data
    - Country  feature : replacing 'fr' by the country variable

---
+++ src/WeatherData.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const config = require('../config/API');
+
+const apiKey = config.owm.key;
+const baseUrl = `https://api.openweathermap.org/data/2.5/weather?APPID=${apiKey}&units=metric`;
+
+module.exports = {
+    getData: async function (city) {
+        const url = baseUrl + `&q=${city.name},${city.country}`.replace(/ /g, '%20')
+        console.log(url)
+        try {
+            const res = await axios.get(url)
+            return res.data
+        } catch (e) {
+            throw e
+        }
+    }
+};
