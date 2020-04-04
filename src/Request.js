089ba013860376dfc84b8169bdb6062c7fe305c1
Nekika <hugo.jahnke@gmail.com>
2020-04-04 19:05:26 +0200

    Refacto
    Deleting external files
    Everything (except the API call) is written in the bot class
    The bot now has a request attribute which is a list of regex that
    represents valid requests to ask

--- src/Request.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    isValid: function (request) {
-        const regex = /^(!weather [a-zA-Z]+([ -_]?[a-zA-Z]+)*|!weather [0-9]{5})(,[a-zA-Z]{2,3})?$/
-        return request.match(regex)
-    }
-};
--- src/Weatherbot.js
+++ src/Weatherbot.js
@@ -1,48 +1,83 @@
 const Discord = require('discord.js')
-const WeatherMessage = require('./WeatherMessage')
-const Request = require('./Request')
 const WeatherDatas = require('./WeatherData')
 
 class Weatherbot extends Discord.Client{
     // Build a Weatherbot
     constructor() {
         super()
+        this.requests = [
+            /help/,                                     // help
+            /[0-9]{5}(,[a-zA-Z]{2})?/,                  // weather by zipcode[,country]
+            /[a-zA-z]+([ -][a-zA-Z]+)*(,[a-zA-Z]{2})?/, // weather by city[,country]
+        ]
     }
 
     // Indicates that the bot is connected to the server and set it's activity
     async init(){
         try {
-            await this.user.setActivity('Type !weather | Made by @Nekika#1693')
+            await this.user.setActivity('Type !weather help | @Nekika#1693')
         } catch (e) {
             throw e
         }
     }
 
-    // Treat a message
-    treat(message){
+    isValid(request) {
+        let validity = false
+        this.requests.forEach(r => { if (request.match(r)) { validity = true} })
+        return validity
+    }
+
+    // Isolates the requets from the command and inspect it
+    inspect(message) {
         const regex = /^!weather .*/
-        if (message.content.match(regex)){
-            const request = message.content
-            if (Request.isValid(request)){
-                this.execute(message)
+        if (message.content.match(regex)) {
+            const cmd = '!weather '
+            const request = message.content.substring(cmd.length)
+            if (this.isValid(request)) {
+                return this.execute(message, request)
             } else {
-                const issue = "Invalid request"
-                this.reject(message, issue)
+                this.reject(message, `woops... unknown command ***${request}*** | type **!weather help** to get some help`)
             }
         }
     }
 
     // Execute a request
-    async execute(request){
-        const command = '!weather '
-        const args = request.content.substring(command.length).split(',')
-        const city = {name: args[0], country: args[1] || 'fr'}
+    async execute(message, request){
+        let response
+        if (request === 'help') {
+            response = this.help()
+        } else {
+            const args = request.split(',')
+            const city = {name: args[0], country: args[1] || 'fr'}
+            response = await this.weather(city)
+        }
+        message.channel.send(response)
+    }
+
+    help() {
+        return new Discord.RichEmbed()
+            .setTitle('Help')
+            .setDescription('**Different ways to get the current weather for a city**')
+            .addField('General use', 'France : *!weather city* | World : *!weather city,country*')
+            .addField('*Tip*', '*[] mean that this part of the command is optionnal*')
+            .addField('Get by Zipcode (most accurate)', 'Example : *!weather 57000[,FR]*')
+            .addField('Get by Name', 'Example : *!weather San Francisco,US*')
+    }
+
+    async weather(city) {
         try {
-            const data =  await WeatherDatas.getData(city)
-            const message = WeatherMessage.create(data)
-            request.channel.send(message)
+            const data = await WeatherDatas.getData(city)
+            console.log(data)
+            return new Discord.RichEmbed()
+                .setTitle(`Weather for ***${data.name}, ${data.sys.country}***`)
+                .addField('\u200B', '\u200B')
+                .setThumbnail(`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+                .addField(':thermometer: Temp', `${data.main.temp} °C`, true)
+                .addField(':thermometer: Feeling', `${data.main.feels_like} °C`, true)
+                .addField('\u200B', '\u200B')
+                .addField(':wind_blowing_face:  Wind', `${data.wind.speed} km/h`, true)
+                .addField(':droplet:  Humidity', `${data.main.humidity} %`, true)
         } catch (e) {
-            this.reject(request, e)
             throw e
         }
     }
